691951b15b4d04f16e43e4d2dfc75e56c8eae163
Leonardo Medina <leo_medina14@hotmail.com>
2026-01-19 00:50:24 -0500

    Add swagger-ui

--- src/main/java/ec/com/ltd/banking/sb/LtdBankingSbApplication.java
+++ src/main/java/ec/com/ltd/banking/sb/LtdBankingSbApplication.java
@@ -1,7 +1,17 @@
 package ec.com.ltd.banking.sb;
 
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import java.util.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LtdBankingSbApplication {
@@ -9,5 +19,27 @@ public class LtdBankingSbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LtdBankingSbApplication.class, args);
 	}
+
+  @Bean @ConditionalOnMissingBean(BuildProperties.class)
+  BuildProperties buildProperties() {
+    return new BuildProperties(new Properties());
+  }
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .components(new Components())
+        .info(new Info()
+            .title("BANKING-TRANSACTIONS REST API Documentation")
+            .description("BANKING-TRANSACTIONS REST API for services")
+            .version(buildProperties().getVersion())
+            .contact(new Contact()
+                .name("Leo Tech Dev")
+                .email("tioleodeveloper@outlook.com")
+                .url("https://github.com/leomedinadev"))
+            .license(new License()
+                .name("Apache 2.0")
+                .url("http://springdoc.org")));
+  }
 
 }
--- src/main/java/ec/com/ltd/banking/sb/infrastructure/web/controller/AccountController.java
+++ src/main/java/ec/com/ltd/banking/sb/infrastructure/web/controller/AccountController.java
@@ -1,6 +1,5 @@
 package ec.com.ltd.banking.sb.infrastructure.web.controller;
 
-import ec.com.ltd.banking.sb.application.dto.AccountDetailsDTO;
 import ec.com.ltd.banking.sb.application.dto.CreateAccountCommand;
 import ec.com.ltd.banking.sb.application.dto.DepositMoneyCommand;
 import ec.com.ltd.banking.sb.application.dto.WithdrawMoneyCommand;
@@ -12,6 +11,10 @@ import ec.com.ltd.banking.sb.infrastructure.web.dto.AccountResponse;
 import ec.com.ltd.banking.sb.infrastructure.web.dto.CreateAccountRequest;
 import ec.com.ltd.banking.sb.infrastructure.web.dto.DepositRequest;
 import ec.com.ltd.banking.sb.infrastructure.web.dto.WithdrawRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/accounts")
 @AllArgsConstructor
+@Tag(name = "Accounts", description = "API para procesos de cuentas")
 public class AccountController {
 
   private final ICreateAccountUseCase createAccountUseCase;
@@ -33,6 +37,12 @@ public class AccountController {
   private final IDepositMoneyUseCase depositMoneyUseCase;
   private final IGetAccountDetailsUseCase getAccountDetailsUseCase;
 
+  @Operation(summary = "Crear cuenta")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Cuenta creada correctamente"),
+      @ApiResponse(responseCode = "400", description = "MethodArgumentNotValidException"),
+      @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+  })
   @PostMapping
   public ResponseEntity<AccountResponse> create(@Valid @RequestBody CreateAccountRequest request) {
     var command = new CreateAccountCommand(request.customerId(), request.initialBalance());
@@ -40,6 +50,12 @@ public class AccountController {
     return ResponseEntity.status(HttpStatus.CREATED).body(AccountResponse.from(dto));
   }
 
+  @Operation(summary = "Depositar dinero en cuenta")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Deposito creado correctamente"),
+      @ApiResponse(responseCode = "400", description = "AccountNotFoundException"),
+      @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+  })
   @PostMapping("/{id}/deposit")
   public ResponseEntity<AccountResponse> deposit(@PathVariable String id, @Valid @RequestBody
       DepositRequest request) {
@@ -48,6 +64,12 @@ public class AccountController {
     return ResponseEntity.ok(AccountResponse.from(dto));
   }
 
+  @Operation(summary = "Retirar dinero de cuenta")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Retiro creado correctamente"),
+      @ApiResponse(responseCode = "400", description = "InsufficientBalanceException"),
+      @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+  })
   @PostMapping("/{id}/withdraw")
   public ResponseEntity<AccountResponse> withdraw(@PathVariable String id, @Valid @RequestBody
       WithdrawRequest request) {
@@ -56,6 +78,12 @@ public class AccountController {
     return ResponseEntity.ok(AccountResponse.from(dto));
   }
 
+  @Operation(summary = "Consultar detalles de cuenta")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Obtener detalles de la cuenta"),
+      @ApiResponse(responseCode = "400", description = "AccountNotFoundException"),
+      @ApiResponse(responseCode = "500", description = "Error interno del servidor")
+  })
   @GetMapping("/{id}")
   public ResponseEntity<AccountResponse> findById(@PathVariable String id) {
     var dto = getAccountDetailsUseCase.getById(id);
